77c619ca7f61ab4e80c2ede9f808b3253ba3e02d
Kyle Gong <kylegong+git@gmail.com>
2020-12-10 23:55:29 -0800

    Speed up d11

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import argparse
 import importlib
 import sys
 from datetime import datetime
 
-if __name__ == "__main__":
-    start = datetime.now()
-    day = sys.argv[1]
-    input_file = "aoc2020/inputs/%s.txt" % day
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('day', type=int, help='day to run')
+parser.add_argument('--year', type=int, default=2020,
+                    help='Advent of Code year')
+parser.add_argument('--time', type=bool, action=argparse.BooleanOptionalAction,
+                    help='show time elapsed')
+parser.add_argument('--profile', type=bool, action=argparse.BooleanOptionalAction,
+                    help='run cProfile')
+
+
+def run_day(day, year):
+    input_file = "aoc%s/inputs/%s.txt" % (year, day)
     with open(input_file) as f:
-        day = importlib.import_module('aoc2020.day%s' % day)
-        day.main(f)
-    print("Ran in %.3fs" % (datetime.now() - start).total_seconds())
+        script = importlib.import_module('aoc%s.day%s' % (year, day))
+        script.main(f)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    start = datetime.now()
+    if args.profile:
+        import cProfile
+        cProfile.run('run_day(args.day, args.year)')
+    run_day(args.day, args.year)
+
+    if args.time:
+        print("Ran in %.3fs" % (datetime.now() - start).total_seconds())
