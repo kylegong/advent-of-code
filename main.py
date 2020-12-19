3e556b1ed156b1063df73cc599684c35f4329663
KG <kylegong+git@gmail.com>
2020-12-19 13:47:05 -0800

    Add test flag to main

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('day', type=int, help='day to run')
 parser.add_argument('--year', type=int, default=2020,
                     help='Advent of Code year')
+parser.add_argument('--test', action="store_true", help='run doctests for day')
 parser.add_argument('--time', action="store_true", help='show time elapsed')
 parser.add_argument('--profile', action="store_true", help='run cProfile')
 
 
-def run_day(day, year):
+def run_day(year, day):
     input_file = "aoc%s/inputs/%s.txt" % (year, day)
     with open(input_file) as f:
         script = importlib.import_module('aoc%s.day%s' % (year, day))
@@ -23,8 +24,17 @@ if __name__ == "__main__":
     start = datetime.now()
     if args.profile:
         import cProfile
-        cProfile.run('run_day(args.day, args.year)')
-    run_day(args.day, args.year)
+        cProfile.run('run_day(args.year, args.day)')
+    elif args.test:
+        import doctest
+        import unittest
+        mod = importlib.import_module('aoc%s.day%s' % (args.year, args.day))
+        tests = unittest.TestSuite()
+        tests.addTests(doctest.DocTestSuite(mod))
+        runner = unittest.TextTestRunner()
+        runner.run(tests)
+    else:
+        run_day(args.year, args.day)
 
     if args.time:
         print("Ran in %.3fs" % (datetime.now() - start).total_seconds())
