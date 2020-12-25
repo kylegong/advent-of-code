a134620f868a99f856c18a1eef17e9f3c75ee642
KG <kylegong+git@gmail.com>
2020-12-25 15:57:01 -0800

    day 24 complete

--- aoc2020/day24.py
+++ aoc2020/day24.py
@@ -1,19 +1,84 @@
+import collections
+
+
 def part1(lines):
-    return [parse(line) for line in lines]
+    locs = [location(parse(line)) for line in lines]
+    return len(visit(locs))
 
 
 def part2(lines):
-    return
+    locs = [location(parse(line)) for line in lines]
+    b = visit(locs)
+    for n in range(100):
+        b = turn(b)
+    return len(b)
 
 
-THETA = {
-    'e': 0,
-    'ne': 60,
-    'nw': 120,
-    'w': 180,
-    'sw': 240,
-    'se': 300,
+def turn(b):
+    w = collections.defaultdict(int)
+    b_flip = set()
+    w_flip = set()
+    for t in b:
+        num_b = 0
+        for n in neighbors(t):
+            if n in b:
+                num_b += 1
+            else:
+                w[n] += 1
+        if num_b == 0 or num_b > 2:
+            b_flip.add(t)
+    for t, c in w.items():
+        if c == 2:
+            w_flip.add(t)
+    return b.difference(b_flip).union(w_flip)
+
+
+COORDS = {
+    'e': (2, 0),
+    'ne': (1, 1),
+    'se': (1, -1),
+    'w': (-2, 0),
+    'sw': (-1, -1),
+    'nw': (-1, 1),
 }
+
+
+def visit(locs):
+    b = set()
+    for l in locs:
+        if l in b:
+            b.remove(l)
+        else:
+            b.add(l)
+    return b
+
+
+def neighbors(loc):
+    for dx, dy in COORDS.values():
+        x, y = loc
+        yield x+dx, y+dy
+
+
+def location(directions):
+    """Translate directions into a canonical location vector
+
+    >>> location(["ne", "se", "e", "e"])
+    (6, 0)
+    >>> location(["ne", "se", "se", "w"])
+    (1, -1)
+    >>> location(["nw", "nw", "se"])
+    (-1, 1)
+    >>> location(["nw", "nw", "ne"])
+    (-1, 3)
+    >>> location(["e", "w", "w", "e", "w"])
+    (-2, 0)
+    """
+    x, y = 0, 0
+    for d in directions:
+        dx, dy = COORDS[d]
+        x += dx
+        y += dy
+    return x, y
 
 
 def parse(line):
@@ -29,7 +94,8 @@ def parse(line):
 
 
 def main(f):
-    lines = [line.strip() for line in EXAMPLE.split("\n")]
+    # f = EXAMPLE.split("\n")
+    lines = [line.strip() for line in f]
     print(part1(lines))
     print(part2(lines))
 
